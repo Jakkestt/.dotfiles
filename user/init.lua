176763e35364f1a7b0690fd19584872c9197d067
Jarkko Arffman <jakkee116@gmail.com>
2023-11-06 00:07:56 +0200

    Change the nvim colorscheme

--- user/init.lua
+++ user/init.lua
@@ -1,4 +1,5 @@
 return {
+  colorscheme = "monokai-pro",
   highlights = {
     -- set highlights for all themes
     -- use a function override to let us use lua to retrieve colors from highlight group
